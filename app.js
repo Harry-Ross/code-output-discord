2c220c0d00a12ba685bfde105b51963c486b584b
Harry Ross <hazross@hotmail.com>
2019-06-19 17:32:38 +1000

    fixed the images for real this time

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+//const client = new Discord.Client();
 
 const express = require('express');
 var app = express();
@@ -17,32 +17,32 @@ const PORT = 3000 || process.env.PORT;
 
 app.use('/static' , express.static("views/static"));
 
-client.once('ready', () => {
-    console.log("ready to do some stupid shit")
-});
+// client.once('ready', () => {
+//     console.log("ready to do some stupid shit")
+// });
 
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+// client.on('message', message => {
+//     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+//     const args = message.content.slice(prefix.length).split(/ +/);
+//     const command = args.shift().toLowerCase();
 
-    if (command == "code") {
-        var lang = args[0];
-        var content = '';
-        for (var i = 1; i < args.length; i++) {
-            content += ' ' + args[i];
-        }
-        var user = message.author.avatar;
-        var id = Math.floor(Math.random() * 1000);
-        code.create({id, language: lang, content, user})
-        message.channel.send(`http://localhost:3000/code/${id}`);
-        console.log(args);
-    }
-})
+//     if (command == "code") {
+//         var lang = args[0];
+//         var content = '';
+//         for (var i = 1; i < args.length; i++) {
+//             content += ' ' + args[i];
+//         }
+//         var user = message.author.avatar;
+//         var id = Math.floor(Math.random() * 1000);
+//         code.create({id, language: lang, content, user})
+//         message.channel.send(`http://localhost:3000/code/${id}`);
+//         console.log(args);
+//     }
+// })
 
 app.listen(PORT, function () {
     console.log("express server running")
 });
 
-client.login(token);
+//client.login(token);
